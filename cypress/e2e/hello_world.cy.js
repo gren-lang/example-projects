ac0f0813ff2ccf8c4243f071684b2c0e9c42d546
Robin Heggelund Hansen <glad.sand2211@heghan.org>
2022-07-01 10:41:00 +0200

    Add test for hello_world project.

--- cypress/e2e/hello_world.cy.js
+++ cypress/e2e/hello_world.cy.js
@@ -1,5 +1,7 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe('Hello world', () => {
+  it('Renders a "Hello, world" string', () => {
+    cy.visit('hello_world/Example.html');
+    
+    cy.get('body div').should('have.text', 'Hello, world!'); 
+  });
+});
